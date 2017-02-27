b12bf48261e0358a843c0963ed4b1413bb757cd6
Andrew Lord <andrewlord1990@gmail.com>
2017-02-27 21:58:43 +0000

    Added “y” versions of extensions methods in RectTransformAnchorExtension.

--- src/ExtensionsPackages/Assets/AndrewLord/ExtensionsPackages/RectTransformAnchor/RectTransformAnchorExtension.cs
+++ src/ExtensionsPackages/Assets/AndrewLord/ExtensionsPackages/RectTransformAnchor/RectTransformAnchorExtension.cs
@@ -16,14 +16,37 @@
       transform.anchorMax = anchorMax;
     }
 
+    public static void SetAnchorMinY(this RectTransform transform, float y) {
+      Vector2 anchorMin = transform.anchorMin;
+      anchorMin.y = y;
+      transform.anchorMin = anchorMin;
+    }
+
+    public static void SetAnchorMaxY(this RectTransform transform, float y) {
+      Vector2 anchorMax = transform.anchorMax;
+      anchorMax.y = y;
+      transform.anchorMax = anchorMax;
+    }
+
     public static void SetAnchorsX(this RectTransform transform, float x) {
       transform.SetAnchorMinX(x);
       transform.SetAnchorMaxX(x);
     }
 
+    public static void SetAnchorsY(this RectTransform transform, float y) {
+      transform.SetAnchorMinY(y);
+      transform.SetAnchorMaxY(y);
+    }
+
     public static void SetAnchoredPositionX(this RectTransform transform, float x) {
       Vector2 anchorPosition = transform.anchoredPosition;
       anchorPosition.x = x;
+      transform.anchoredPosition = anchorPosition;
+    }
+    
+    public static void SetAnchoredPositionY(this RectTransform transform, float y) {
+      Vector2 anchorPosition = transform.anchoredPosition;
+      anchorPosition.y = y;
       transform.anchoredPosition = anchorPosition;
     }
   }
